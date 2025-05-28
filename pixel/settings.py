37a23149c18fe62dfea98ec8aab21a814c0cf4e5
Cleriston Martins Cardoso <46527150+cleristonmartins9102@users.noreply.github.com>
2025-05-28 13:19:32 +0100

    feat: added swagger

--- pixel/settings.py
+++ pixel/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
-    'api'
+    'api',
+    'drf_yasg'
 ]
 AUTH_USER_MODEL = 'api.UserModel'
 
@@ -82,18 +83,27 @@ WSGI_APPLICATION = 'pixel.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'library',
+#         'USER': 'dbadmin',
+#         'PASSWORD': '123456',
+#         'HOST': 'library-db',   # ou IP do host do banco
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'library',
-        'USER': 'dbadmin',
-        'PASSWORD': '123456',
-        'HOST': 'library-db',   # ou IP do host do banco
+        'NAME': 'books',
+        'USER': 'meuusuario',
+        'PASSWORD': 'minhasenha',
+        'HOST': '127.0.0.1', 
         'PORT': '5432',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
--- pixel/urls.py
+++ pixel/urls.py
@@ -1,10 +1,27 @@
-from django.urls import path, include
-
+from django.urls import path, re_path, include
+from rest_framework import permissions
 from api.views import listOfBooksView
 from api.views import loginView
 from api.views import listOfBorrowBooksView, adminView, usersAdminView, booksAdminView
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Library API",
+      default_version='v1',
+      description="API documentation for the Library app",
+      terms_of_service="https://www.example.com/terms/",
+      contact=openapi.Contact(email="contact@example.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 urlpatterns = [
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', adminView),
     path('admin/books', booksAdminView),
     path('admin/users', usersAdminView),
